06e1f9d0f70d3a7b01a787f199aa4328e6b24331
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-03-28 10:01:42 +1300

    Add files via upload

---
+++ 00_FRC_Base_V1.py
@@ -0,0 +1,20 @@
+
+
+# functions go here
+def num_check (question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+
+
+
+
+
+
+        except ValueError:
+
+# main routine goes here
+get_int = num_check("How many do you need? ", "Please enter an amount more than 0\n"int)
+
+get_cost = num_check("How much does it cost? $", "Please enter a number more than 0\n"float)
